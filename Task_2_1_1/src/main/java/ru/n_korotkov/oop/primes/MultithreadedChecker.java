dc0140db7f73632e8be8a54b6bd763f83e567b8c
Nikita Korotkov <n.korotkov@g.nsu.ru>
2022-06-16 22:34:59 +0700

    MultithreadedChecker refactor

--- Task_2_1_1/src/main/java/ru/n_korotkov/oop/primes/MultithreadedChecker.java
+++ Task_2_1_1/src/main/java/ru/n_korotkov/oop/primes/MultithreadedChecker.java
@@ -3,13 +3,10 @@ package ru.n_korotkov.oop.primes;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MultithreadedChecker extends PrimeChecker {
-
-    private class State {
-        private int finishedTasks = 0;
-        private boolean result = false;
-    }
 
     int threads;
 
@@ -21,33 +18,28 @@ public class MultithreadedChecker extends PrimeChecker {
         if (numbers.isEmpty())
             return false;
 
-        State state = new State();
+        AtomicBoolean nonPrimeFound = new AtomicBoolean(false);
 
         ExecutorService execService = Executors.newFixedThreadPool(threads);
-        var taskStream = numbers
-            .stream()
-            .map(x -> (Runnable)(() -> {
-                boolean isPrime = checkPrime(x);
-                synchronized (state) {
-                    state.result |= !isPrime;
-                    state.finishedTasks++;
-                    if (!isPrime || state.finishedTasks == numbers.size()) {
-                        state.notify();
+        for (int i = 0; i < threads; i++) {
+            int taskN = i;
+            execService.execute(() -> {
+                for (int index = taskN; index < numbers.size() && !nonPrimeFound.get(); index += threads) {
+                    boolean isPrime = checkPrime(numbers.get(index));
+                    if (!isPrime) {
+                        nonPrimeFound.set(true);
                     }
                 }
-            }));
-
-        synchronized (state) {
-            try {
-                taskStream.forEach(execService::execute);
-                state.wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            execService.shutdownNow();
-            return state.result;
+            });
         }
+
+        try {
+            execService.shutdown();
+            execService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return nonPrimeFound.get();
     }
 
 }
