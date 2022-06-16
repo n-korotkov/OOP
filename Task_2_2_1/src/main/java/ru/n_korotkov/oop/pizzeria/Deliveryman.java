44bab73198df80c61a9cc4d3f12caa2153e64bf5
Nikita Korotkov <n.korotkov@g.nsu.ru>
2022-06-16 23:48:43 +0700

    Added SynchronizedQueue.takeAtMost method

--- Task_2_2_1/src/main/java/ru/n_korotkov/oop/pizzeria/Deliveryman.java
+++ Task_2_2_1/src/main/java/ru/n_korotkov/oop/pizzeria/Deliveryman.java
@@ -1,7 +1,5 @@
 package ru.n_korotkov.oop.pizzeria;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import ru.n_korotkov.oop.pizzeria.PizzeriaConfig.DeliverymanConfig;
@@ -18,23 +16,11 @@ public class Deliveryman implements Runnable {
         this.storage = storage;
     }
 
-    private List<Integer> takeOrders() throws InterruptedException {
-        List<Integer> takenOrders = new ArrayList<>(bootCapacity);
-        takenOrders.add(storage.take());
-        while (takenOrders.size() < bootCapacity) {
-            Integer nextOrder = storage.poll();
-            if (nextOrder == null) break;
-
-            takenOrders.add(nextOrder);
-        }
-        return takenOrders;
-    }
-
     @Override
     public void run() {
         try {
             while (true) {
-                var orders = takeOrders();
+                var orders = storage.takeAtMost(bootCapacity);
                 String orderString = orders.stream().map(Object::toString).collect(Collectors.joining(", "));
                 System.out.printf("[%s] taken from storage\n", orderString);
 
